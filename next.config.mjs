07a168ac485007e6d1467e68a14058bdbbaa8559
paraspipre <paras11917@gmail.com>
2024-09-26 10:41:32 +0530

    contact form

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,12 @@
 const nextConfig = {
     images: {
       domains: ['images.unsplash.com'], // Allow images from Unsplash
-    },
+  },
+  env: {
+    NEXT_PUBLIC_EMAIL_USERNAME:"secret.account@outlook.com",
+    NEXT_PUBLIC_EMAIL_PASSWORD : "password123",
+    NEXT_PUBLIC_PERSONAL_EMAIL : "myemail@gmail.com",
+  }
   };
   
   export default nextConfig;
